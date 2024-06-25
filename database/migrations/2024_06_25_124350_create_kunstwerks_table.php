a3947072784c7babbe658a1000c5b056227797d6
Sven van Rijn <vanrijn@cardsolutions.nl>
2024-06-25 15:24:45 +0200

    init commit

---
+++ database/migrations/2024_06_25_124350_create_kunstwerks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kunstwerken', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam');
+            $table->string('omschrijving');
+            $table->string('afbeelding_path');
+            $table->unsignedBigInteger('kunstenaar_id');
+            $table->foreign('kunstenaar_id')->references('id')->on('kunstenaars');
+            $table->int('prijs');
+            $table->date('datum');
+            $table->string('locatie');
+            $table->string('dimensies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kunstwerks');
+    }
+};
